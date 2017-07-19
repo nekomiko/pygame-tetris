bd3c61a8cd9a0582af5b702a8fa36de2b81565c0
Reimu Neko Miko <nekomiko444@gmail.com>
2017-07-19 04:24:33 +0300

    Tetris: documentating and small refactoring

--- tetris.py
+++ tetris.py
@@ -1,10 +1,14 @@
-from grid import Grid
-from game import Score
 import random
 import time
+import operator
+
 import pygame
 from pygame.locals import *
-FIGURES_unproc = [
+
+from grid import Grid
+from game import Score
+
+_FIGURES_unproc = [
     [
         ["X", "X"],
         ["X", "X"]
@@ -33,6 +37,7 @@ FIGURES_unproc = [
 
 
 def rotate_figure(figure):
+    '''Returns rotated figure, presented as 2d-array'''
     n = len(figure)
     m = len(figure[0])
     r_figure = [[" " for i in range(n)] for j in range(m)]
@@ -43,17 +48,20 @@ def rotate_figure(figure):
 
 
 def preprocess_figures(figures_unproc):
-    figures = [[] for i in range(len(figures_unproc))]
-    for i in range(len(figures_unproc)):
-        f_rot = figures[i]
-        figure = figures_unproc[i]
+    '''Converts an array of figures to an array of arrays of length 4
+    of all rotations of corresponding figures'''
+    def all_rotations(figure):
+        f_rot = []
         for j in range(0, 4):
             f_rot.append(figure)
             figure = rotate_figure(figure)
+        return f_rot
+
+    figures = list(map(all_rotations, figures_unproc))
     return figures
 
 
-FIGURES = preprocess_figures(FIGURES_unproc)
+_FIGURES = preprocess_figures(_FIGURES_unproc)
 
 
 class TetrisGameOver(Exception):
@@ -61,6 +69,21 @@ class TetrisGameOver(Exception):
 
 
 class TetrisModel:
+    '''Model of tetris game
+    self.grid: rectangular grid of playing field, Grid object
+    self.placed_cells: 2-d array of placed pieces of figures,
+        element is pygame Color of the cell
+    self.grid_size: dimensions of self.placed_cells
+    format of figure: 2d-array in FIGURE format
+    self.next_figure: figure text to appear
+    self.current_figure: figgure currently playing
+    self.current_figure_pos position of figure on a field
+    self.score: players score
+    self.last_active: unix-time of last turn-based move
+    self.status: status of the game:
+        "game_active" for running game
+        "game_over" for ended game
+    '''
     cell_color = "#FF00FF"
     move_delay = 0.250
 
@@ -69,10 +92,12 @@ class TetrisModel:
         self.reinit_round()
 
     def reinit_round(self):
+        '''Reinitilizes game after start or gameover'''
         self.next_figure = self.random_figure()
-        size = self.grid.size
+        self.grid_size = self.grid.size
         self.placed_cells = [
-            [None for y in range(size[1])] for x in range(size[0])]
+            [None for y in range(self.grid.size[1])]
+            for x in range(self.grid.size[0])]
         self.take_next_figure()
         self.score = 0
         self.last_active = time.time()
@@ -80,16 +105,18 @@ class TetrisModel:
         self.status = "game_active"
 
     def pause_toggle(self):
+        '''Sets game on pause'''
         self.pause_status = not self.pause_status
         self.last_active = time.time()
 
     def random_figure(self):
-        fig_i = random.randint(0, len(FIGURES) - 1)
+        '''Returns random tetris figue'''
+        fig_i = random.randint(0, len(_FIGURES) - 1)
         rot_i = random.randint(0, 4 - 1)
-        return FIGURES[fig_i][rot_i]
+        return _FIGURES[fig_i][rot_i]
 
     def take_next_figure(self):
-        '''Replaces current figure with new next random'''
+        '''Replaces current playing figure with next'''
         next_figure = self.random_figure()
         figure_pos = (
             (self.grid.size[0] - len(self.next_figure)) // 2, 0)
@@ -97,43 +124,46 @@ class TetrisModel:
             raise TetrisGameOver
         self.current_figure = self.next_figure
         self.next_figure = next_figure
-        self.figure_pos = figure_pos
+        self.current_figure_pos = figure_pos
 
     def rotation_move(self):
-        '''Rotates current figure'''
+        '''Rotates current playing figure'''
         new_figure = rotate_figure(self.current_figure)
-        if self.collides(new_figure, self.figure_pos):
+        if self.collides(new_figure, self.current_figure_pos):
             return False
         else:
             self.current_figure = new_figure
             return True
 
     def collides(self, figure, pos):
-        '''Checks collision of figure at the position (pos) with the occupied cells'''
+        '''Checks collision of `figure` at `pos` with the occupied cells'''
         for i in range(len(figure)):
             for j in range(len(figure[0])):
                 if figure[i][j] != " ":
-                    in_range_i = (
-                        pos[0] + i >= 0) and (pos[0] + i < self.grid.size[0])
-                    in_range_j = (
-                        pos[1] + j >= 0) and (pos[1] + j < self.grid.size[1])
-                    if (not in_range_i) or (not in_range_j) or self.placed_cells[pos[0] + i][pos[1] + j]:
+                    shift_pos = tuple(map(operator.add, pos, (i, j)))
+                    in_range_i = (shift_pos[0] >= 0) and \
+                        (shift_pos[0] < self.grid_size[0])
+                    in_range_j = (shift_pos[1] + j >= 0) and \
+                        (shift_pos[1] < self.grid_size[1])
+                    if (not in_range_i) or (not in_range_j) or \
+                            self.placed_cells[shift_pos[0]][shift_pos[1]]:
                         return True
         return False
 
     def make_next_step(self):
+        '''Proceed to next step of turn-based game'''
         def get_full_line():
-            for j in range(len(self.placed_cells[0])):
+            for j in range(self.grid_size[1]):
                 full = True
-                for i in range(len(self.placed_cells)):
-                    if not self.placed_cells[i][j]:
+                for i in range(self.grid_size[0]):
+                    if self.placed_cells[i][j] is None:
                         full = False
                 if full:
                     return j
             return None
 
         def remove_line(line_num):
-            for i in range(len(self.placed_cells)):
+            for i in range(self.grid_size[0]):
                 self.placed_cells[i][0] = None
                 for j in range(line_num - 1, 0, -1):
                     self.placed_cells[i][j + 1] = self.placed_cells[i][j]
@@ -152,24 +182,25 @@ class TetrisModel:
             for i in range(len(figure)):
                 for j in range(len(figure[0])):
                     if figure[i][j] != " ":
-                        self.placed_cells[pos[0] + i][pos[1] + j] = color
+                        self.placed_cells[pos[0] + i][pos[1] + j] = self.cell_color
 
-        color = self.cell_color
-        if not self.move((0, 1)):
-            place_piece(self.current_figure, self.figure_pos)
+        if not self.move_figure((0, 1)):
+            place_piece(self.current_figure, self.current_figure_pos)
             remove_full_lines()
             self.take_next_figure()
 
-    def move(self, dir):
-        new_pos = (self.figure_pos[0] + dir[0], self.figure_pos[1] + dir[1])
+    def move_figure(self, dir):
+        '''Moves current playing figure in direction'''
+        new_pos = tuple(map(operator.add, self.current_figure_pos, dir))
         if self.collides(self.current_figure, new_pos):
             return False
         else:
-            self.figure_pos = new_pos
+            self.current_figure_pos = new_pos
             return True
 
     def propagate(self):
-        '''Handles time, gamestatuses and calls model update'''
+        '''Continue process game, should be called on each frame.abs
+        Handles time, gamestatuses and calls model update'''
         c_time = time.time()
         if self.status == "game_active" and (not self.pause_status):
             try:
@@ -182,16 +213,18 @@ class TetrisModel:
             self.reinit_round()
 
     def draw(self):
-        for i in range(len(self.placed_cells)):
-            for j in range(len(self.placed_cells[0])):
-                if(self.placed_cells[i][j]):
+        '''Draws game objects on grid'''
+        for i in range(self.grid.size[0]):
+            for j in range(self.grid.size[1]):
+                if(self.placed_cells[i][j] is not None):
                     self.grid.set_cell_state((i, j), self.placed_cells[i][j])
 
         for i in range(len(self.current_figure)):
             for j in range(len(self.current_figure[1])):
                 if(self.current_figure[i][j] != " "):
-                    self.grid.set_cell_state(
-                        (self.figure_pos[0] + i, self.figure_pos[1] + j), self.cell_color)
+                    shift_pos = tuple(map(operator.add,
+                                          self.current_figure_pos, (i, j)))
+                    self.grid.set_cell_state(shift_pos, self.cell_color)
 
 
 class TetrisGame:
@@ -232,11 +265,11 @@ class TetrisGame:
                     if event.key == K_UP or event.key == K_w:
                         self.game.rotation_move()
                     if event.key == K_LEFT or event.key == K_a:
-                        self.game.move((-1, 0))
+                        self.game.move_figure((-1, 0))
                     if event.key == K_RIGHT or event.key == K_d:
-                        self.game.move((1, 0))
+                        self.game.move_figure((1, 0))
                     if event.key == K_DOWN or event.key == K_s:
-                        self.game.move((0, 1))
+                        self.game.move_figure((0, 1))
                     if event.key == K_SPACE or event.key == K_p:
                         self.game.pause_toggle()
 
